2d7ec15631ce436c25c1cfdbf75bf204c28c6d80
tuananh250696 <35196486+tuananh250696@users.noreply.github.com>
2018-08-27 23:55:52 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -112,7 +112,39 @@ angular.module('myApp', [
 	// draw line chart
 	new Chart(buyers).Line(buyerData);
 	
+	// pie chart options
+	var pieOptions = {
+		segmentShowStroke : false,
+		animateScale : true
+	}
+	// get pie chart canvas
+	var countries= document.getElementById("countries").getContext("2d");
 	
+	// draw pie chart
+	new Chart(countries).Pie(pieData, pieOptions);
+		// bar chart data
+	var barData = {
+	labels : ["monday","Tuesday","wednesday","thursday","friday","satuday","sunday"],
+	datasets : [
+		{
+			fillColor : "#48A497",
+			strokeColor : "#48A4D1",
+			data : [456,479,324,569,702,600]
+		},
+		{
+			fillColor : "rgba(73,188,170,0.4)",
+			strokeColor : "rgba(72,174,209,0.4)",
+			data : [364,504,605,400,345,320]
+		}
+		]
+	}
+	
+	// get bar chart canvas
+	var income = document.getElementById("income").getContext("2d");
+	
+	// draw bar chart
+	new Chart(income).Bar(barData);
+
 	
 	
 	
