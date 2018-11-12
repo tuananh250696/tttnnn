fc8325b8c3ed23db75b8a3de22c6e0e46dfc593d
tuananh250696 <35196486+tuananh250696@users.noreply.github.com>
2018-11-12 22:58:59 +0700

    Update webapp.js

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -14,16 +14,10 @@ angular.module('myApp', [
 		ioSocket: myIoSocket
 	});
 	return mySocket;
-	
-/////////////////////// Những dòng code ở trên phần này là phần cài đặt, các bạn hãy đọc thêm về angularjs để hiểu, cái này không nhảy cóc được nha!
 }).controller('Home', function($scope, mySocket) {
 	////Khu 1 -- Khu cài đặt tham số 
     //cài đặt một số tham số test chơi
 	//dùng để đặt các giá trị mặc định
-    $scope.CamBienMua = "chưa có cap nhat";
-    $scope.leds_status = [1, 1]
-	$scope.lcd = ["", ""]
-	$scope.servoPosition = 0
 	$scope.buttons = [] //chả có gì cả, arduino gửi nhiêu thì nhận nhiêu!
 	
 	////Khu 2 -- Cài đặt các sự kiện khi tương tác với người dùng
@@ -43,29 +37,9 @@ angular.module('myApp', [
 		mySocket.emit("LED", json)
 	}
 	
-	//cập nhập lcd như một ông trùm 
-	$scope.updateLCD = function() {
-		
-		
-		var json = {
-			"line": $scope.lcd
-		}
-		console.log("LCD_PRINT ", $scope.lcd)
-		mySocket.emit("LCD_PRINT", json)
-	}
 	
-	//Cách gửi tham số 2: dùng biến cục bộ: servoPosition. Biến này đươc truyền từ file home.html, dữ liệu đươc truyền vào đó chính là biến toàn cục $scope.servoPosition. Cách 2 này sẽ giúp bạn tái sử dụng hàm này vào mục đích khác, thay vì chỉ sử dụng cho việc bắt sự kiện như cách 1, xem ở Khu 4 để biết thêm ứng dụng!
-	$scope.updateServo = function(servoPosition) {
-		
-		var json = {
-			"degree": servoPosition,
-			"message": "Goc ne: " + servoPosition
-		}
-		
-		console.log("SEND SERVO", json) //debug chơi à
-		mySocket.emit("SERVO", json)
-	}
 	
+
 	 $scope.upt5  = function(){
 	var ip = document.getElementById('ip').value;
 		mySocket.emit("LED1ON"+ip)
@@ -105,103 +79,8 @@ angular.module('myApp', [
 		console.log("recv BUTTON", json)
 		$scope.buttons = json.data
 	})
-	
-	//
-	
-	// line chart data
-	var buyerData = {
-		labels : ["monday","Tuesday","wednesday","thursday","friday","satuday","sunday"],
-		datasets : [
-		{
-				fillColor : "rgba(172,194,132,0.4)",
-				strokeColor : "#ACC26D",
-				pointColor : "#fff",
-				pointStrokeColor : "#9DB86D",
-				data : [203,156,99,251,305,247,200]
-				
-			}
-		]
-	}
-	
-	var buyers = document.getElementById('buyers').getContext('2d');
 
-	// draw line chart
-	new Chart(buyers).Line(buyerData);
-	
-	// pie chart data
-	var pieData = [
-		{
-			value: 20,
-			color:"#878BB6"
-		},
-		{
-			value : 40,
-			color : "#4ACAB4"
-		},
-		{
-			value : 10,
-			color : "#FF8153"
-		},
-		{
-			value : 30,
-			color : "#FFEA88"
-		}
-	];
-	// get line chart canvas
-	var buyers = document.getElementById('buyers').getContext('2d');
 
-	// draw line chart
-	new Chart(buyers).Line(buyerData);
-	
-	// pie chart options
-	var pieOptions = {
-		segmentShowStroke : false,
-		animateScale : true
-	}
-	// get pie chart canvas
-	var countries= document.getElementById("countries").getContext("2d");
-	
-	// draw pie chart
-	new Chart(countries).Pie(pieData, pieOptions);
-		// bar chart data
-	var barData = {
-	labels : ["monday","Tuesday","wednesday","thursday","friday","satuday","sunday"],
-	datasets : [
-		{
-			fillColor : "#48A497",
-			strokeColor : "#48A4D1",
-			data : [456,479,324,569,702,600]
-		},
-		{
-			fillColor : "rgba(73,188,170,0.4)",
-			strokeColor : "rgba(72,174,209,0.4)",
-			data : [364,504,605,400,345,320]
-		}
-		]
-	}
-	
-	// get bar chart canvas
-	var income = document.getElementById("income").getContext("2d");
-	
-	// draw bar chart
-	new Chart(income).Bar(barData);
-
-	
-	
-	
-	
-/////
-	////Khu 3 -- Nhận dữ liệu từ Arduno gửi lên (thông qua ESP8266 rồi socket server truyền tải!)
-	//các sự kiện từ Arduino gửi lên (thông qua esp8266, thông qua server)
-	mySocket.on('RAIN', function(json) {
-		$scope.CamBienMua = (json.digital == 1) ? "Không mưa" : "Có mưa rồi yeah ahihi"
-	})
-	//Khi nhận được lệnh LED_STATUS
-	mySocket.on('LED_STATUS', function(json) {
-		//Nhận được thì in ra thôi hihi.
-		console.log("recv LED", json)
-		$scope.leds_status = json.data
-	})
 	//khi nhận được lệnh Button
 	mySocket.on('BUTTON', function(json) {
 		//Nhận được thì in ra thôi hihi.
